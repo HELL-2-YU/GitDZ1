8e037fdcba3c450b80574c0892f88f97b03118a6
Kamchatniy Andrei <kamchatnyj.av@gmail.com>
2024-09-13 22:22:56 +0100

    Add capitalize_words function and docstring for to_uppercase

---
+++ str_func.py
@@ -0,0 +1,22 @@
+def capitalize_words(input_string):
+    """
+    Делает заглавными первые буквы каждого слова в строке.
+
+    Args:
+        input_string (str): Строка, которую нужно преобразовать.
+
+    Returns:
+        str: Строка, где первые буквы каждого слова заглавные.
+    """
+    return input_string.title()
+def to_uppercase(input_string):
+    """
+    Преобразует все символы строки в верхний регистр.
+
+    Args:
+        input_string (str): Строка, которую нужно преобразовать.
+
+    Returns:
+        str: Строка, содержащая все символы в верхнем регистре.
+    """
+    return input_string.upper()
