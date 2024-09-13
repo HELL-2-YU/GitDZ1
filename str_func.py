4b7098fb3a248de99886773e4da7c1f151fbf6fd
HELL-2-U <kamchatnyj.av@gmail.com>
2024-09-13 21:06:38 +0100

    Add docstring str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def to_uppercase(input_string):
+    """
+    Преобразует все символы строки в верхний регистр.
+
+    Args:
+        input_string (str): Строка, которую нужно преобразовать.
+
+    Returns:
+        str: Строка, содержащая все символы в верхнем регистре.
+    """
     return input_string.upper()
